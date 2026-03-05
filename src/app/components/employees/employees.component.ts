841872b3e799ef37db67c8172e76ab7989a57e0c
Ramu Y <ramuy@prospectatech.com>
2026-03-05 18:33:21 +0530

    4th commit

--- src/app/components/employees/employees.component.ts
+++ src/app/components/employees/employees.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Employee } from 'src/app/models/employee';
+import { ApiService } from 'src/app/services/api.service';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-employees',
@@ -7,4 +10,31 @@ import { Component } from '@angular/core';
 })
 export class EmployeesComponent {
 
+  employees: Employee[] = [];
+  router: any;
+  constructor(private apiService: ApiService, private authService: AuthService) { }
+
+  ngOnInit() {
+    this.loadEmployees();
+  }
+
+  loadEmployees(): void {
+    this.apiService.request('GET', '/employees')
+      .subscribe({
+        next: (res: any) => {
+          this.employees = res.employees || [];
+          console.log('Employees:', this.employees);
+        },
+        error: (err) => {
+          if (err.status === 401) {
+            alert('Failed to load employees');
+            this.authService.setLoginStatus(false);
+          } else {
+            alert('Something went wrong. Please try again');
+          }         
+          
+          console.error('Failed to load employees', err);
+        }
+      });
+  }
 }
---
+++ src/app/models/employee.ts
@@ -0,0 +1,9 @@
+export interface Employee {
+    emp_number: string;
+    employee_id: string;
+    emp_firstname: string;
+    emp_lastname: string;
+    emp_birthday: string;
+    emp_mobile: string;
+    emp_work_email: string;
+}
