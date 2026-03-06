1ba5a4eadcff4055ef60239239d501dc3117ffce
Ramu Y <ramuy@prospectatech.com>
2026-03-06 18:05:26 +0530

    5t commit

--- src/app/components/employees/employees.component.ts
+++ src/app/components/employees/employees.component.ts
@@ -1,21 +1,43 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 import { Employee } from 'src/app/models/employee';
 import { ApiService } from 'src/app/services/api.service';
 import { AuthService } from 'src/app/services/auth.service';
+declare var $: any;
 
 @Component({
   selector: 'app-employees',
   templateUrl: './employees.component.html',
   styleUrls: ['./employees.component.css']
 })
-export class EmployeesComponent {
+export class EmployeesComponent implements OnInit, AfterViewInit {
 
   employees: Employee[] = [];
   router: any;
+
   constructor(private apiService: ApiService, private authService: AuthService) { }
 
   ngOnInit() {
     this.loadEmployees();
+  }
+
+  ngAfterViewInit() {
+
+    setTimeout(() => {
+
+      if ($.fn.DataTable.isDataTable('#employeeTable')) {
+        $('#employeeTable').DataTable().destroy();
+      }
+
+      $('#employeeTable').DataTable(
+        {
+          dom: 'Bfrtip',
+          buttons: [
+            'excelHtml5',
+            'pdfHtml5'
+          ]
+        }
+      );
+    }, 300);
   }
 
   loadEmployees(): void {
@@ -23,6 +45,9 @@ export class EmployeesComponent {
       .subscribe({
         next: (res: any) => {
           this.employees = res.employees || [];
+          setTimeout(() => {
+            $('#employeeTable').DataTable();
+          }, 200);
           console.log('Employees:', this.employees);
         },
         error: (err) => {
@@ -31,8 +56,8 @@ export class EmployeesComponent {
             this.authService.setLoginStatus(false);
           } else {
             alert('Something went wrong. Please try again');
-          }         
-          
+          }
+
           console.error('Failed to load employees', err);
         }
       });
--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from 'src/app/services/api.service';
+import { AuthService } from 'src/app/services/auth.service';
+declare var $: any;
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit, AfterViewInit{
+
+  useres: any[] = [];
+
+  constructor(private apiService: ApiService, private authService: AuthService) { }
+
+  ngOnInit() {
+    this.loadUseres();
+  }
+
+    ngAfterViewInit() {
+
+    setTimeout(() => {
+
+      if ($.fn.DataTable.isDataTable('#userTable')) {
+        $('#userTable').DataTable().destroy();
+      }
+
+      $('#userTable').DataTable(
+        {
+          dom: 'Bfrtip',
+          buttons: [
+            'excelHtml5',
+            'pdfHtml5'
+          ]
+        }
+      );
+    }, 300);
+  }
+
+  loadUseres(): void {
+    this.apiService.request('GET', '/useres').subscribe({
+      next: (res: any) => {
+        this.useres = res.useres || [];
+      },
+      error: (err: any) => {
+        if (err.status === 401) {
+          alert('Failed to load users');
+          this.authService.setLoginStatus(false);
+        } else {
+          alert('Something went wrong. Please try again');
+        }
+      }
+    });
+  }
 
 }
