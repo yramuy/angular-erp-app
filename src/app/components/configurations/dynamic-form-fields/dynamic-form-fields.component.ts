fef44012c685a8db7e0c8058c569851194fe8f53
Ramu Y <ramuy@prospectatech.com>
2026-03-13 18:50:54 +0530

    7th commit

--- src/app/components/configurations/dynamic-form-fields/dynamic-form-fields.component.ts
+++ src/app/components/configurations/dynamic-form-fields/dynamic-form-fields.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-dynamic-form-fields',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./dynamic-form-fields.component.css']
 })
 export class DynamicFormFieldsComponent {
+
+  formFields: any[] = [];
+  isMessage: boolean = false;
+  message: string = "";
+
+  constructor(private apiService: ApiService){}
+
+  ngOnInit() {
+    this.loadDynamicFormFields();
+  }
+
+  loadDynamicFormFields() {
+    this.apiService.request('GET', '/allDynamicFormFields').subscribe({
+      next: (res: any) => {
+        this.formFields = res.formFields || [];
+      },
+      error: (err) => {
+        if(err.status === 401) {
+          this.showMessage("Unauthorized request");
+        } else {
+          this.showMessage("Server error, something went wrong, please try again");
+        }
+      }
+    });
+  }
+
+  showMessage(msg: string) {
+    this.message = msg;
+    this.isMessage = true;
+
+    setTimeout(() => {
+      this.isMessage = false;
+    }, 3000);
+  }
 
 }
